b2a5e81c0247810d1cc7a00e9bc788d207f58418
unknown <senguptasaumyajeet@outlook.com>
2022-09-06 17:01:55 +0530

    third commit

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor() {
+        super(); /* Since we are inheriting properties from React and in inheritance if I call the child class before calling parent then the properties of parent's won't be accessible to the child hence we call parent first and then we call the child constructor */
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
     render() {
+        const { price, title, qty } = this.state; /* Object destructuring */
         return (
             <div className='cart-item'>
                 <div className='left-block'>
                     <img style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className='cart-item-actions'>
                         {/* BUTTONS HERE */}
+                        <img alt="increase" className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992651.png' />
+                        <img alt="decrease" className='action-icons' src='https://cdn-icons-png.flaticon.com/512/992/992683.png' />
+                        <img alt="delete" className='action-icons' src='https://cdn-icons-png.flaticon.com/512/1214/1214428.png' />
                     </div>
                 </div>
             </div>
